8d405252010cb5f3b3f4819527ea33fa6bca7c1a
youyou <1094465220@qq.com>
2019-08-29 23:22:57 +0800

    crud

--- src/main/java/tws/controller/EmployeeController.java
+++ src/main/java/tws/controller/EmployeeController.java
@@ -1,17 +1,16 @@
 package tws.controller;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tws.entity.Employee;
 import tws.repository.EmployeeMapper;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -21,9 +20,38 @@ public class EmployeeController {
     @Autowired
     private EmployeeMapper employeeMapper;
 
-    @GetMapping("")
+    @GetMapping
     public ResponseEntity<List<Employee>> getAll() {
+
         return ResponseEntity.ok(employeeMapper.selectAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getOne(@PathVariable String id) {
+
+        Employee employee = employeeMapper.selectOne(id);
+        return ResponseEntity.ok(employee);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateOne(@RequestBody Employee employee,@PathVariable String id) {
+        employee.setId(id);
+        employeeMapper.updateOne(employee,id);
+        return ResponseEntity.ok(employee);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Employee> deleteOne(@PathVariable String id){
+        employeeMapper.deleteOne(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+
+    }
+
+
+
+    @PostMapping
+    public ResponseEntity<Employee> insertEmployee(@RequestBody Employee employee) {
+        String id = UUID.randomUUID().toString();
+        employee.setId(id);
+        employeeMapper.insertOne(employee);
+        return ResponseEntity.created(URI.create("/employees"+employee.getId())).body(employee);
     }
 
 }
